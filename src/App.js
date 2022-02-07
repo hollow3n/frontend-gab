8906193c045f80fddc1e0613031877b28b679785
Philippe Sendze <sendze.philippe@gmail.com>
2022-02-07 18:46:08 +0100

    Commit tardif datant de vendredi. Fonctionnalités minimales du GAB

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import DemandeTransfert from './components/demandeTransfert';
+import ReadTransfert from './components/readTransfert';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="main">
+        <h1>GAB</h1>
+        <div>
+          <Route exact path='/' component={DemandeTransfert} />
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <Route exact path='/read' component={ReadTransfert} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
